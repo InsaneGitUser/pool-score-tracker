e30943eacd82483ef266bb9a6766ce503c92f6ef
InsaneGitUser <epikduck@duck.com>
2026-04-18 23:39:52 -0600

    Update pool_webkit.c

--- pool_webkit.c
+++ pool_webkit.c
@@ -682,6 +682,7 @@ int main(int argc, char *argv[])
 
     gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(webview));
     gtk_widget_show_all(window);
+    gtk_window_fullscreen(GTK_WINDOW(window));
 
     gtk_main();
     return 0;
